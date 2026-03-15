df53f03c5dd32d6e3c501e6f751d6317dff692e5
Dinho <dinhoimbr3@gmail.com>
2026-03-15 22:30:57 +0100

    feat: implement Task domain (entity, repository, service, controller, tests)

---
+++ src/main/java/com/paulo/workorder/entity/Task.java
@@ -0,0 +1,50 @@
+package com.paulo.workorder.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(String description, String title) {
+        this.description = description;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
